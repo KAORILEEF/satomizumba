82330174db002ef659985dbc27def11193bb1a25
KAORILEEF <128810953+KAORILEEF@users.noreply.github.com>
2025-12-28 12:04:22 +0900

    Update zumba.js

--- js/zumba.js
+++ js/zumba.js
@@ -27,6 +27,15 @@
 
     /* スライド変化にかける時間を保持する変数 */
     var durationSlideFade = 1000;
+    // ドット
+var $dotArea = $(".slide-dots");
+
+for(var i = 0; i < slideNum; i++){
+  $dotArea.append("<span></span>");
+}
+
+var $dots = $dotArea.find("span");
+$dots.eq(0).addClass("active");
 
 
     /* --- 初期設定パート --- */
@@ -41,12 +50,17 @@
     /* --- 関数定義パート --- */
 
     /* 次のスライドを表示する */
-    function showNextSlide(){
-        var nextSlide = (nowSlide + 1) % slideNum;
-        
-        $slideContents.eq(nowSlide).fadeOut(durationSlideFade);//imgから今の番号を取得して1000ミリ秒でフェードアウト
-        $slideContents.eq(nextSlide).fadeIn(durationSlideFade);//imgから次の番号を取得して1000ミリ秒でフェードイン
+function showNextSlide(){
+  var nextSlide = (nowSlide + 1) % slideNum;
 
-        nowSlide = nextSlide;//次の番号を今の番号にする
-    }
+  $slideContents.eq(nowSlide).fadeOut(durationSlideFade);
+  $slideContents.eq(nextSlide).fadeIn(durationSlideFade);
+
+  // ★ ドット制御
+  $dots.removeClass("active");
+  $dots.eq(nextSlide).addClass("active");
+
+  nowSlide = nextSlide;
+}
+
 });
